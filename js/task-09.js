66dfab63460b3ce609d2eb9d587a4e79284bdd4a
manataliekeithse <141150584+manataliekeithse@users.noreply.github.com>
2024-03-08 11:56:37 +0800

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,8 +1,3 @@
- function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215)
-      .toString(16)
-      .padStart(6, '0')}`;
-  }
 
   const colorSpan = document.querySelector('.color');
   const changeColorButton = document.querySelector('.change-color');
