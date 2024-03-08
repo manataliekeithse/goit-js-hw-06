2e09962a9c96d91a97cf641f35ee916c1bbf02ae
manataliekeithse <141150584+manataliekeithse@users.noreply.github.com>
2024-03-08 11:54:51 +0800

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,14 +1,15 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+ function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, '0')}`;
+  }
 
-let span = document.querySelector(".color");
-let btn = document.querySelector(".change-color");
+  const colorSpan = document.querySelector('.color');
+  const changeColorButton = document.querySelector('.change-color');
+  const body = document.querySelector('body');
 
-btn.addEventListener("click", () => {
-  let color = getRandomHexColor;
-  span.innerText = color;
-  document.body.style.backgroundColor = color;
-});
+  changeColorButton.addEventListener('click', function() {
+    const newColor = getRandomHexColor();
+    body.style.backgroundColor = newColor;
+    colorSpan.textContent = newColor;
+  });
